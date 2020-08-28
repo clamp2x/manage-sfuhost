403c96797181db864e6ee1cc1694735dd929738a
clamp2x <clamp2x@hotmail.com>
2020-08-29 02:37:47 +0900

    Update php_cli_selector.sh
    경로 수정

--- ShellScript/php_cli_selector.sh
+++ ShellScript/php_cli_selector.sh
@@ -3,7 +3,7 @@
 # Copyright (C) 2020 Study For Us HOSTING (https://hosting.studyforus.com)
 # Changing PHP cli version via jailkit for each users.
 # This script is able to use only on ispconfig
-# Version information : 0.2 (Proto type)
+# Version information : 0.2.1 (Proto type)
 # License : The MIT License (MIT)
 
 
@@ -48,17 +48,17 @@ done
 
 
 # php 버전이 설치 되어 있나 확인
-if [ ! -f /usr/bin/$phpversion ]; then
+if [ ! -f /var/www/clients/client%cn/web$wn/usr/bin/$phpversion ]; then
   echo "There is not php version to change."
   echo "Start to copy php version to change."
-  jk_init -c /etc/jailkit/jk_init.ini -f -k -j /var/www/clients/client1/web1 $phpversion
+  jk_init -c /etc/jailkit/jk_init.ini -f -k -j /var/www/clients/client%cn/web$wn $phpversion
 fi
 
 # php altenative 버전 삭제
-rm /var/www/client%cn/web$wn/etc/alternatives/php
+rm /var/www/clients/client%cn/web$wn/etc/alternatives/php
 
 # php 버전 변경
-ln -s /usr/bin/$phpversion /var/www/client%cn/web$wn/etc/alternatives/php
+ln -s /usr/bin/$phpversion /var/www/clients/client%cn/web$wn/etc/alternatives/php
 
 # 완료.
 echo "php cli version has been changed."
