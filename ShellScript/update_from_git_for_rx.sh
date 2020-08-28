d95816da4198d3e7f04bcd40f425e2d272b9909c
clamp2x <clamp2x@hotmail.com>
2020-08-29 01:30:49 +0900

    Create update_from_git_for_rx.sh

---
+++ ShellScript/update_from_git_for_rx.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+ 
+# Copyright (C) 2017 Study For Us HOSTING (https://hosting.studyforus.com)
+# Auto Copying Tool From Git Repository
+# Version infomation : 0.1 (Proto type)
+# License : The MIT License (MIT)
+ 
+# Define Vars
+GIT_DIR='git' #Input only FOLDER NAME
+WEB_DIR='/web' #Input Web Directory (both absolute path and relative path are possible)
+GIT_SOURCE='https://github.com/rhymix/rhymix/' #Input Git repository path
+ 
+# Checking Git clone
+if [ ! -d $WEB_DIR/$GIT_DIR/.git ]; then
+  echo "Git Directory does not exist. Git Directory will be made."
+  echo "Start git clone progress"
+  mkdir -p $WEB_DIR/$GIT_DIR
+  git clone $GIT_SOURCE $WEB_DIR/$GIT_DIR/
+fi
+ 
+ 
+# Checking updates and patches from Git repository
+cd $WEB_DIR/$GIT_DIR
+git pull
+ 
+# Copying Git files to Web Directory by rsync
+rsync -avr --exclude='$GIT_DIR' $WEB_DIR/$GIT_DIR/ $WEB_DIR/
