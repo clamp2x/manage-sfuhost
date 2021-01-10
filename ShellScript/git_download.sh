c25d294709b0a84a74902afb471f01cf33070c6b
clamp2x <clamp2x@hotmail.com>
2021-01-10 13:47:20 +0900

    Create git_download.sh
    가이드만 잡음

---
+++ ShellScript/git_download.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Copyright (C) 2017 Study For Us HOSTING (https://hosting.studyforus.com)
+# auto git download and updater
+# Version infomation : 0.0.1 
+# License : The MIT License (MIT)
+
+
+# 변수 설정
+# 깃 다운로드 주소
+# 깃 이름
+# 깃 다운로드 폴더
+# 깃 브랜치 설정 (차후 추가)
+
+
+# git 다운로드 폴더에서 .git 폴더가 있는지 확인
+
+# .git 폴더가 없는 경우 git clone 실행
+
+# git 클론 후 rsync 로 복사
+
+# 원본 git 폴더 제거
+
+
+
+
+# .git 폴더가 있는 경우 업데이트
+
+# 변경 내용이 있는지 확인
+
+# 변경 내용 원상 복귀 후 업데이트
+
+# 변경 내용 다시 적용
