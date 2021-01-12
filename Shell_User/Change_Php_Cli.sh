5522fc042a07019900248ef3f60327504973fbb4
clamp2x <clamp2x@hotmail.com>
2021-01-13 02:42:33 +0900

    Update Change_Php_Cli.sh
    version changed.

--- Shell_User/Change_Php_Cli.sh
+++ Shell_User/Change_Php_Cli.sh
@@ -3,7 +3,7 @@
 # Copyright (C) 2020 Study For Us HOSTING (https://hosting.studyforus.com)
 # Changing PHP cli version via jailkit for each users.
 # This script is able to use only on ispconfig
-# Version information : 1.2.0
+# Version information : 1.3.1
 # License : The MIT License (MIT)
 
 # 화면 클리어
